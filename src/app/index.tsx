5c0b5a46404973f7dc3ef7567f1c9d61d91a652f
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2026-03-29 13:23:51 +0630

    hide back button on top

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,32 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+  
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Hello World</Text>
+      <TextInput 
+        placeholder="youremail"
+        style={{
+          borderWidth:1,padding:10
+        }}
+        secureTextEntry={false}  
+      />
+      <TextInput 
+        placeholder="password" 
+        style={{
+          borderWidth:1,padding:10
+        }}
+        secureTextEntry={true}  
+      />
+      <TouchableOpacity>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
+      <Link href="/about">Visit About Screen</Link>
     </View>
   );
 }
@@ -13,5 +36,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    flex:1,
+    width: '100%',
+    backgroundColor: '#0553',
   },
 });
